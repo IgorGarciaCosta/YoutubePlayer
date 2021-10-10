ff102950e1c29d411832b243a95b24502a9cde73
IgorGarciaCosta <igonildo7@gmail.com>
2021-10-09 23:33:41 -0300

    Setting up connection to youtube API

--- app/src/main/java/com/example/youtubeplayer/YoutubeActivity.kt
+++ app/src/main/java/com/example/youtubeplayer/YoutubeActivity.kt
@@ -2,14 +2,36 @@ package com.example.youtubeplayer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.google.android.youtube.player.YouTubeBaseActivity
 import com.google.android.youtube.player.YouTubeInitializationResult
 import com.google.android.youtube.player.YouTubePlayer
+import com.google.android.youtube.player.YouTubePlayerView
 
+const val YOUTUBE_VIDEO_ID = "H1DEczvTjgE&list=PLV8TJ3QA8lRoDx_jhs6toHwXUB_d8hUQD&index=1"
+const val YOUTUBE_PLAYLIST="PLV8TJ3QA8lRoDx_jhs6toHwXUB_d8hUQD"
 class YoutubeActivity : YouTubeBaseActivity(), YouTubePlayer.OnInitializedListener {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_youtube)
+        //setContentView(R.layout.activity_youtube)
+        //val layout = findViewById<ConstraintLayout>(R.id.activity_youtube)
+        val layout = layoutInflater.inflate(R.layout.activity_youtube, null) as ConstraintLayout
+        setContentView(layout)
+
+        //val button1 = Button(this)
+        //button1.layoutParams = ConstraintLayout.LayoutParams(600, 180)
+        //button1.text = "Test button"
+        //layout.addView(button1)
+
+        val playerView = YouTubePlayerView(this)
+        playerView.layoutParams = ConstraintLayout.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+
+        layout.addView((playerView))
     }
 
     override fun onInitializationSuccess(
