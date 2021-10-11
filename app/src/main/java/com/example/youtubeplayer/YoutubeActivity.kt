1d37363ae54ce6c39bf0592083ce2d4c8836d3e9
IgorGarciaCosta <igonildo7@gmail.com>
2021-10-11 11:26:30 -0300

    Added working player to layout and fixed height error

--- app/src/main/java/com/example/youtubeplayer/YoutubeActivity.kt
+++ app/src/main/java/com/example/youtubeplayer/YoutubeActivity.kt
@@ -1,17 +1,20 @@
 package com.example.youtubeplayer
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.google.android.youtube.player.YouTubeBaseActivity
 import com.google.android.youtube.player.YouTubeInitializationResult
 import com.google.android.youtube.player.YouTubePlayer
 import com.google.android.youtube.player.YouTubePlayerView
 
-const val YOUTUBE_VIDEO_ID = "H1DEczvTjgE&list=PLV8TJ3QA8lRoDx_jhs6toHwXUB_d8hUQD&index=1"
-const val YOUTUBE_PLAYLIST="PLV8TJ3QA8lRoDx_jhs6toHwXUB_d8hUQD"
+const val YOUTUBE_VIDEO_ID = "ElpitAfkRS4"
+const val YOUTUBE_PLAYLIST="PLXtTjtWmQhg1SsviTmKkWO5n0a_-T0bnD"
 class YoutubeActivity : YouTubeBaseActivity(), YouTubePlayer.OnInitializedListener {
 
 
@@ -29,23 +32,37 @@ class YoutubeActivity : YouTubeBaseActivity(), YouTubePlayer.OnInitializedListen
 
         val playerView = YouTubePlayerView(this)
         playerView.layoutParams = ConstraintLayout.LayoutParams(
-            ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+            ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
 
-        layout.addView((playerView))
+        layout.addView(playerView)
+
+        playerView.initialize(getString(R.string.GOOGLE_API_KEY), this)
     }
 
     override fun onInitializationSuccess(
-        p0: YouTubePlayer.Provider?,
-        p1: YouTubePlayer?,
-        p2: Boolean
+        provider: YouTubePlayer.Provider?,
+        youtubePlayer: YouTubePlayer?,
+        wasRestored: Boolean
     ) {
+        Log.d(TAG, "On init success")
+        Toast.makeText(this, "successfully init", Toast.LENGTH_SHORT).show()
 
+        if(!wasRestored){
+            youtubePlayer?.cueVideo(YOUTUBE_VIDEO_ID)
+        }
     }
 
     override fun onInitializationFailure(
-        p0: YouTubePlayer.Provider?,
-        p1: YouTubeInitializationResult?
+        provider: YouTubePlayer.Provider?,
+        youTubeInitializationResult: YouTubeInitializationResult?
     ) {
-        TODO("Not yet implemented")
+        val REQUEST_CODE = 0
+        if(youTubeInitializationResult?.isUserRecoverableError==true){
+            youTubeInitializationResult.getErrorDialog(this, REQUEST_CODE).show()
+        }
+        else{
+            val errorMsg = "Error initializing ytPlayer"
+            Toast.makeText(this, errorMsg, Toast.LENGTH_LONG).show()
+        }
     }
 }
