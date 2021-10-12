ed7a92202485bb4c2875d35177ae070d5352ed20
IgorGarciaCosta <igonildo7@gmail.com>
2021-10-12 17:00:53 -0300

    Stantong to add functionalities to the buttons of the stand alone activity

---
+++ app/src/main/java/com/example/youtubeplayer/StandAloneActivity.kt
@@ -0,0 +1,51 @@
+package com.example.youtubeplayer
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.youtube.player.YouTubeStandalonePlayer
+import com.google.android.youtube.player.YouTubeThumbnailLoader
+import kotlinx.android.synthetic.main. activity_standalone.*
+
+class StandAloneActivity:AppCompatActivity(), View.OnClickListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_standalone)
+
+        btnPlayVideo.setOnClickListener(this)
+        btnPlaylist.setOnClickListener(this)
+
+        //btnPlayVideo.setOnClickListener(object: View.OnClickListener{
+        //   override fun onClick(p0: View?) {
+        //        TODO("Not yet implemented")
+        //    }
+        //})
+
+        //btnPlayVideo.setOnClickListener(View.OnClickListener { v->
+
+        //})
+
+
+        //val listener = View.OnClickListener { v->
+        //}
+        //btnPlayVideo.setOnClickListener(listener)
+        //btnPlaylist.setOnClickListener(listener)
+
+
+    }
+
+    override fun onClick(view: View) {
+        val intent = when(view.id){
+            R.id.btnPlayVideo->YouTubeStandalonePlayer.createVideoIntent(
+                this, getString(R.string.GOOGLE_API_KEY), YOUTUBE_VIDEO_ID
+            )
+            R.id.btnPlaylist->YouTubeStandalonePlayer.createPlaylistIntent(
+                this, getString(R.string.GOOGLE_API_KEY), YOUTUBE_PLAYLIST
+            )
+            else-> throw IllegalArgumentException("Undefigned button clicked")
+        }
+
+        startActivity(intent)
+    }
+}
